98b84aaa28520a06cdb7379bc0963d0a46b3945a
gramalhodevops <50681238+gramalhodevops@users.noreply.github.com>
2020-12-08 09:10:43 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -12,13 +12,13 @@ exports.logExecuteData = [];
 function logData(req) {
     exports.logExecuteData.push({
         body: req.body,
-        headers: JSON.stringify(req.headers),
-        trailers: JSON.stringify(req.trailers),
+        headers: req.headers,
+        trailers: req.trailers,
         method: req.method,
         url: req.url,
         params: req.params,
         query: req.query,
-        route: JSON.stringify(req.route),
+        route: req.route,
         cookies: req.cookies,
         ip: req.ip,
         path: req.path,
@@ -30,13 +30,13 @@ function logData(req) {
         originalUrl: req.originalUrl
     });
     console.log("body: " + util.inspect(req.body));
-    console.log("headers: " + req.headers);
-    console.log("trailers: " + req.trailers);
+    console.log("headers: " + JSON.stringify(req.headers));
+    console.log("trailers: " + JSON.stringify(req.trailers));
     console.log("method: " + req.method);
     console.log("url: " + req.url);
     console.log("params: " + util.inspect(req.params));
     console.log("query: " + util.inspect(req.query));
-    console.log("route: " + req.route);
+    console.log("route: " + JSON.stringify(req.route));
     console.log("cookies: " + req.cookies);
     console.log("ip: " + req.ip);
     console.log("path: " + req.path);
@@ -74,30 +74,12 @@ exports.save = function (req, res) {
  * POST Handler for /execute/ route of Activity.
  */
 exports.execute = function (req, res) {
-
-    // example on how to decode JWT
-    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
-
-        // verification error -> unauthorized request
-        if (err) {
-            console.error(err);
-            return res.status(401).end();
-        }
-
-        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-            
-            // decoded in arguments
-            var decodedArgs = decoded.inArguments[0];
-            
-            logData(req);
-            res.send(200, 'Execute');
-        } else {
-            console.error('inArguments invalid.');
-            return res.status(400).end();
-        }
-    });
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body )
+    logData(req);
+    //res.send(200, 'Execute');
+    res.status(200).send('Execute');
 };
-
 
 /*
  * POST Handler for /publish/ route of Activity.
